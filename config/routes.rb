bae2ccd50db12832fd19862e44e7c591f35d7605
Rodrigue Danso <onecodeaday.ocad@gmail.com>
2024-08-28 15:54:51 +0100

    Fix a typo in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resouces :clients
+  resources :clients
 
   resources :orders
 
   resources :item_carts
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
