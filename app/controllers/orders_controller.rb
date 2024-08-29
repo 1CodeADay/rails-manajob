cc98aadd524580cdd91d251014d0f4ce13691abb
Amine Mohamed Fattah <fattah.am93@gmail.com>
2024-08-29 10:57:37 +0100

    add method to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,43 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
   def index
+    @orders = Order.all
   end
 
   def new
+    @order = Order.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to orders_path(@order)
+  end
+
+  def update
+    @order.update(order_params)
+    redirect_to order_path(@order)
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to orders_path, status: :see_other
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
