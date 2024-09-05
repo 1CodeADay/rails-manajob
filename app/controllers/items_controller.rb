ee6d70e8d2e7daf5a0995522fbd4918113ea07c3
Amine Mohamed Fattah <fattah.am93@gmail.com>
2024-09-05 15:36:12 +0100

    create order

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,24 @@
 class ItemsController < ApplicationController
 
+  # POST /items
+  #
+  # Creates a new Item and associated Order and ItemCart. Redirects to the created item.
+  #
+  # The order is created with status "pending" and associated with the client and current user.
+  # If the order already exists, the last one is picked.
+  # The item is associated with the order and item cart.
+  #
+  # Redirects to the created item with a notice.
   def create
     @item = Item.new(item_params)
-    # create order and item cart for new item
-    # verify if order created or not
-    # if order created, create item cart
-    # else create new order and item cart and item
-
-    
     if @item.save!
-      redirect_to @item
+      if Order.where(client_id: params["client"], status: "pending", user_id: current_user.id).exists?
+        ordre = Order.where(client_id: params["client"], status: "pending", user_id: current_user.id).last
+      else
+        ordre = Order.create!(client_id: params["client"], status: "pending", user_id: current_user.id)
+      end
+      ItemCart.create!(order_id: ordre.id, item_id: @item.id, deadline: params["deadline"], status: "pending")
+      redirect_to item_path(@item), :notice => "Successfully created item."
     end
   end
 
