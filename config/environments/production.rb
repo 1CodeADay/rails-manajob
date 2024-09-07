7e0381f2d682bfbd41239ce899a8af0e414a4e96
Rodrigue Danso <onecodeaday.ocad@gmail.com>
2024-09-07 11:53:41 +0100

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.assets.css_compressor = nil
 
