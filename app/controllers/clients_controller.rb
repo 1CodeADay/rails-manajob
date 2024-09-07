d31e43a89fa94a6bcd4550f35beed44c1fbbf8e1
Amine Mohamed Fattah <fattah.am93@gmail.com>
2024-09-07 03:47:09 +0100

    show order and clients + edit clients + final adjustments

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -37,7 +37,7 @@ class ClientsController < ApplicationController
   end
 
   def update
-    @client.update(params[:client])
+    @client.update(client_params)
     redirect_to client_path(@client)
   end
 
